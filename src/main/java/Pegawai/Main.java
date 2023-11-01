2b2eaf1a56c6defd392363b418e33d94f89aa9c5
AndiSyafarIlham <2211103046@ittelkom-pwt.ac.id>
2023-11-01 09:19:14 +0700

    Update Laporan

--- src/main/java/Pegawai/Main.java
+++ src/main/java/Pegawai/Main.java
@@ -1,4 +1,23 @@
 package Pegawai;
 
 public class Main {
+    public static void main(String[] args) {
+        pegawai karyawanZ , karyawanX;
+        karyawanZ = new pegawai();
+        karyawanX = new pegawai();
+
+        System.out.println();
+        karyawanZ.setNIP("2211103046");
+        karyawanZ.setNama("Andi Syafar Ilham");
+        karyawanZ.setAlamat("Purwokerto");
+        karyawanZ.setInfo(12,5000000);
+        karyawanZ.cetak();
+        System.out.println();
+
+        karyawanX.setNIP("2211103044");
+        karyawanX.setNama("Andhika W");
+        karyawanX.setAlamat("Jawa Tengah");
+        karyawanX.setInfo(4500000);
+        karyawanX.cetak();
+    }
 }
--- src/main/java/Pegawai/Pegawai.java
+++
@@ -1,4 +0,0 @@
-package Pegawai;
-
-public class Pegawai {
-}
